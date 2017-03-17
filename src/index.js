6fbdf8016e576220ac4e7d58065b1655f78bf508
Janne Lönn <janne@izettle.com>
2017-03-17 21:56:44 +0100

    Add babel with presets and default options

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import axios from "axios"
 
 class ServiceFactory {
-  static baseUrl(host) {
-    return new ServiceFactory(host)
+  static baseUrl(host, defaults = {}) {
+    return new ServiceFactory(host, defaults)
   }
 
-  constructor(host) {
+  constructor(host, defaults) {
     this.client = axios.create({ baseURL: host })
+    this.defaults = defaults
+  }
+
+  withDefaults(config) {
+    this.defaults = config
+    return this
+  }
+
+  requestOptions(options) {
+    return Object.assign(this.defaults, options)
   }
 
   create(declaration) {
     return Object.keys(declaration).reduce((acc, current) => {
-      return Object.assign(acc, { [current]: () => this.client(declaration[current]) })
+      return Object.assign(acc, {
+        [current]: () => this.client(this.requestOptions(declaration[current]))
+      })
     }, {})
   }
 }
