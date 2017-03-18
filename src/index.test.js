686e83a6662b38468c25457e5ffd7d1e9dde241f
Janne Lönn <janne@izettle.com>
2017-03-18 10:40:50 +0100

    Add POST request test

--- src/index.test.js
+++ src/index.test.js
@@ -8,6 +8,10 @@ const declaration = {
   allTodos: {
     method: "GET",
     url: "/todos"
+  },
+  createTodo: {
+    method: "POST",
+    url: "/todos"
   }
 }
 
@@ -18,6 +22,22 @@ test("ServiceFactory create()", (t) => {
 })
 
 test("ServiceFactory created service GET request", (t) => {
+  t.plan(1)
+  moxios.install()
+  const service = ServiceFactory.baseUrl("www.foo.com").create(declaration)
+  const expected = "hello"
+  moxios.stubRequest("www.foo.com/todos", {
+    status: 200,
+    responseText: "hello"
+  })
+  const callback = sinon.spy()
+  service.allTodos().then(callback)
+  moxios.wait(() => {
+    t.equal(callback.getCall(0).args[0].data, "hello", "should be able to call service with GET request")
+  })
+})
+
+test("ServiceFactory created service POST request", (t) => {
   t.plan(1)
   moxios.install()
   const service = ServiceFactory.baseUrl("www.foo.com").create(declaration)
