18a4319083de20889355577edce1c6dd58e78e01
Janne Lönn <janne@izettle.com>
2017-03-18 10:40:26 +0100

    Add get request test

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,34 @@
 import test from "tape"
+import moxios from "moxios"
+import sinon from "sinon"
+
 import ServiceFactory from "./index"
+
+const declaration = {
+  allTodos: {
+    method: "GET",
+    url: "/todos"
+  }
+}
 
 test("ServiceFactory create()", (t) => {
   t.plan(1)
-  const declaration = {
-    allTodos: {
-      method: "GET",
-      url: "/todos"
-    }
-  }
   const service = ServiceFactory.baseUrl("www.foo.com").create(declaration)
   t.equal(typeof service.allTodos, "function", "should create service with allTodos function")
+})
+
+test("ServiceFactory created service GET request", (t) => {
+  t.plan(1)
+  moxios.install()
+  const service = ServiceFactory.baseUrl("www.foo.com").create(declaration)
+  const expected = "hello"
+  moxios.stubRequest("www.foo.com/todos", {
+    status: 200,
+    responseText: "hello"
+  })
+  const callback = sinon.spy()
+  service.allTodos().then(callback)
+  moxios.wait(() => {
+    t.equal(callback.getCall(0).args[0].data, "hello", "should be able to call service with GET request")
+  })
 })
